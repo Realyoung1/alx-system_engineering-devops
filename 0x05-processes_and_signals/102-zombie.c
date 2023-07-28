ddc063af160c406f12640c4ccc31fb619b982087
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-07-28 14:53:34 -0700

    Update 102-zombie.c

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,8 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
 
 /**
  * infinite_while - Run an infinite while loop.
