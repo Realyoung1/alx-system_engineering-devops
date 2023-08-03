1d4b73b9f72060b65919794a94ea02ca2838995d
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-03 13:07:45 -0700

    Update 0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
 # Simply matching School..
+# The regular expressions must match "School"
+# Using the project instructions to create a Ruby script that accepts one args
 
 puts ARGV[0].scan(/School/).join
