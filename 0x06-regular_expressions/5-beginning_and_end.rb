2fd9e00f2adc31c18e902dd2ca0e9060a4481166
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-03 13:18:34 -0700

    Update 5-beginning_and_end.rb

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,2 +1,6 @@
 #!/usr/bin/env ruby
-#puts ARGV[0].scan(/^h.n$/).join
+# Not quite HBTN yet
+# The regular expression must be exactly matching a string that starts with h ends with n and can have any single character in between
+# Using the project instructions, create a Ruby script that accepts one argument and pass it to a regular expression matching method
+
+puts ARGV[0].scan(/^h.n$/).join
