9e12f126bf75960302e104b13535ee801bc88fe8
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-03 13:22:07 -0700

    Update 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,9 +1,8 @@
 #!/usr/bin/env ruby
 # Textme 
 # Your script should output: [SENDER],[RECEIVER],[FLAGS]
-
-    The sender phone number or name (including country code if present)
-    The receiver phone number or name (including country code if present)
-    The flags that were used
+# The sender phone number or name (including country code if present)
+# The receiver phone number or name (including country code if present)
+# The flags that were used
 
 puts ARGV[0].scan(/(?<=from|to|flags):(\+?\w+|[-?[0-1]:?]+)/).join(',')
