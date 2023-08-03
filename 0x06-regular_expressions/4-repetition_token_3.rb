00cdbd02756e47e7c89879b81f52466f138b1995
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-03 13:16:54 -0700

    Update 4-repetition_token_3.rb

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,2 +1,7 @@
 #!/usr/bin/env ruby
-#puts ARGV[0].scan(/hbt*n/).join
+# Repetition Token #3
+# Finding the regular expression that will match the above cases.
+# Using the project instructions to create a Ruby scripts that accepts one args.
+# Your regex should not contain square brackets.
+
+puts ARGV[0].scan(/hbt*n/).join
