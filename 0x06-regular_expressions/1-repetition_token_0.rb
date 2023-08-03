34cfc5a17bcfdf3e7bd80743212aea8f6cbabceb
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-03 13:04:58 -0700

    Update 1-repetition_token_0.rb

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 # Repetition Token #0
 # Finding the regular expressions..
+# Using the project instructions, create a Ruby script that accepts one argument
 
 puts ARGV[0].scan(/hbt{2,5}n/).join
