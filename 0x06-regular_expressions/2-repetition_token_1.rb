db05eb49997aeb6f77802056a8a661f793f5a2c9
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-03 13:09:59 -0700

    Update 2-repetition_token_1.rb

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,2 +1,6 @@
 #!/usr/bin/env ruby
-#puts ARGV[0].scan(/hb{1}?tn/).join
+# Repetition Token #1
+# Find the regular expression that will match the above cases.
+# Using the project instructions to create a Ruby script that accepts one args..
+
+puts ARGV[0].scan(/hb{1}?tn/).join
