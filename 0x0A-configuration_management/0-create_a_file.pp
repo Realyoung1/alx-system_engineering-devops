6845ee9a70b8bfbedc505a484b24dc4403df02f1
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-09-22 10:08:19 -0700

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
-# creates a file in /tmp
-
+# Create a file
 file { '/tmp/school':
-  content =>'I love Puppet',
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-}
+  content => 'I love Puppet',
