7ba14ac37d0f80c3ccc70de0d0fbe9bb731868c9
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-09-27 00:40:54 +0100

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,17 +4,17 @@ package { 'nginx':
   name   => 'nginx',
 }
 
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-  path    => '/var/www/html/index.html'
-}
-
 file_line { 'title':
   ensure   => present,
   path     => '/etc/nginx/sites-available/default',
   after    => 'server_name _;',
   line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
   multiple => true
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+  path    => '/var/www/html/index.html'
 }
 
 service { 'nginx':
