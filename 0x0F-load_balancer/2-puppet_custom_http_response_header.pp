9b0711211dfd6f49a7eb1ccc7cb305fbf2ded7d3
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-10-02 22:12:29 +0100

    F

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -15,4 +15,11 @@ file_line { 'headercustom':
   ensure  => present,
   path    => '/etc/nginx/sites-available/default',
   after   => ':80 default_server;',
-  line    => "add_header X-Served
+  line    => "add_header X-Served-By ${hostname};",
+  require => Package['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => File_line['headercustom'],
+}
