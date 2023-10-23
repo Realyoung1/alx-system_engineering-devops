93f7c42375514074a9e85e8552c6bd2e0ac5dac6
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-10-23 09:38:13 -0700

    afi

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
-"""Exports to-do list information of all employees to JSON format."""
+"""
+    all tasls from the employee was recodrded
+    i used the format give
+    i used the names given todo_all_employees.json
+"""
 import json
 import requests
+from sys import argv
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users").json()
-
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            u.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
-            } for t in requests.get(url + "todos",
-                                    params={"userId": u.get("id")}).json()]
-            for u in users}, jsonfile)
+    url_employ = "https://jsonplaceholder.typicode.com/users"
+    r_employs = requests.get(url_employ).json()
+    report = {}
+    for employ in r_employs:
+        id_em = employ.get("id")
+        url_todos = url_employ + "/{}/todos".format(id_em)
+        r_todos = requests.get(url_todos).json()
+        username = employ.get("username")
+        total_num_task = r_todos
+        list_dict_report = []
+        for task in total_num_task:
+            id_report = {}
+            id_report["username"] = str(username)
+            id_report["completed"] = task.get("completed")
+            id_report["task"] = str(task.get("title"))
+            list_dict_report.append(id_report)
+        report[id_em] = list_dict_report
+    with open("todo_all_employees.json", "w") as fjson:
+        fjson.write(json.dumps(report))
