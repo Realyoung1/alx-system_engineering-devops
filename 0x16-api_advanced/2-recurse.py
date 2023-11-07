4918a63423761bd80b5cf3660423da47ebddd24e
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-11-07 22:57:38 +0100

    Adv Api

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,23 +1,29 @@
-es the reddit API for get the all hot posts
+#!/usr/bin/python3
+"""
+    I Ensure that you are
+    not following redirects.
+    Prototype:
+    def recurse(subreddit, hot_list=[])
+    none returned coz not a valid subreddit
 """
 import requests
 from sys import argv
 
 
 def recurse(subreddit, hot_list=[], after=""):
-        """Get the all host posts"""
-            if after is None:
-                    return []
+    """Get the all host posts"""
+    if after is None:
+        return []
 
-                        url_sred_inf = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-                            url_sred_inf += "?limit=100&after={}".format(after)
-                                headers = {'user-agent': 'request'}
-                                    response = requests.get(url_sred_inf, headers=headers,
-                                                                allow_redirects=False)
-                                                                    if str(response) != "<Response [200]>":
-                                                                            return None
-                                                                                r_json = response.json()
-                                                                                    hot_posts_json = r_json.get("data").get("children")
-                                                                                        for post in hot_posts_json:
-                                                                                                hot_list.append(post.get("data").get("title"))
-                                                                                                    return hot_list + recurse(subreddit, [], r_json.get("data").get("after"))
+    url_sred_inf = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url_sred_inf += "?limit=100&after={}".format(after)
+    headers = {'user-agent': 'request'}
+    response = requests.get(url_sred_inf, headers=headers,
+                            allow_redirects=False)
+    if str(response) != "<Response [200]>":
+        return None
+    r_json = response.json()
+    hot_posts_json = r_json.get("data").get("children")
+    for post in hot_posts_json:
+        hot_list.append(post.get("data").get("title"))
+    return hot_list + recurse(subreddit, [], r_json.get("data").get("after"))
