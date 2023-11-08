12c31d4ce8d490365864620012f3e236ee151429
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-11-08 15:07:28 -0800

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,8 @@
-# Fix settin
+# Strace is my good friend
+# i found why Apache is returning a 500 error
+# I fixed the issue and the automated it
+# I used tmux to run strace in one window
+# Strace was attaced to current working process
 exec { 'settingPress':
   command  => 'sed -i "s/\b.phpp\b/.php/g" /var/www/html/wp-settings.php',
   provider => shell,
