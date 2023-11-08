0d04091e7124dc466e045dcf2d489956de552409
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-11-08 15:08:14 -0800

    fix wordpress

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,6 +3,7 @@
 # I fixed the issue and the automated it
 # I used tmux to run strace in one window
 # Strace was attaced to current working process
+
 exec { 'settingPress':
   command  => 'sed -i "s/\b.phpp\b/.php/g" /var/www/html/wp-settings.php',
   provider => shell,
